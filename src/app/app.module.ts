2b325746330aacc693f5905b235c21bbab346b94
Steven <stevenzh2020@gmail.com>
2021-01-11 18:02:05 +0800

    added new reservation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,43 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewReservationComponent } from './components/new-reservation/new-reservation.component';
 import { ReservationComponent } from './components/reservation/reservation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
+import { MaterialModule } from './material.module';
+
+import {
+  MAT_MOMENT_DATE_FORMATS,
+  MomentDateAdapter,
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+} from '@angular/material-moment-adapter';
+
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+} from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    AppComponent,
-    NewReservationComponent,
-    ReservationComponent
-  ],
+  declarations: [AppComponent, NewReservationComponent, ReservationComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'en-SG' },
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/new-reservation/new-reservation.component.ts
+++ src/app/components/new-reservation/new-reservation.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+interface Car {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-new-reservation',
   templateUrl: './new-reservation.component.html',
-  styleUrls: ['./new-reservation.component.scss']
+  styleUrls: ['./new-reservation.component.scss'],
 })
 export class NewReservationComponent implements OnInit {
+  selectedValue?: string;
+  selectedCar?: string;
 
-  constructor() { }
+  foods: Food[] = [
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' },
+  ];
 
-  ngOnInit(): void {
-  }
+  cars: Car[] = [
+    { value: 'volvo', viewValue: 'Volvo' },
+    { value: 'saab', viewValue: 'Saab' },
+    { value: 'mercedes', viewValue: 'Mercedes' },
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
