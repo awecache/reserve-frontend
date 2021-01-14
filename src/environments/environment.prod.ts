824b4f67791a64c6d2af2e9f971dc0e543488fbe
Steven <stevenzh2020@gmail.com>
2021-01-15 06:01:49 +0800

    fixed bugs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   googleClientId:
     '1094217161012-kg2f0kd85mkbqql2htd3ilni02evmvh7.apps.googleusercontent.com',
+  // '624796833023-clhjgupm0pu6vgga7k5i5bsfp6qp6egh.apps.googleusercontent.com',
 };
